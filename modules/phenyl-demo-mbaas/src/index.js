5a11e7e3a40510abcc2428a6ce529ee56c32174a
saji-ryu <rsajiki@gmail.com>
2018-06-08 12:03:56 +0900

    理解のためのコメントを追加

--- modules/phenyl-demo-mbaas/src/index.js
+++ modules/phenyl-demo-mbaas/src/index.js
@@ -39,17 +39,26 @@ const main = async () => {
         throw new Error(`Environment variable 'PORT' must be integer: ${String(process.env.PORT)}`)
     }
 
-    //PhenylrestAPIを作成するためのfunctionGroupe
+    //PhenylrestAPIを作成するためのfunctionGroupe　今は空
     const functionalGroup = {}
+    // phenylsessionclientが何してるかわからない....
     const sessionClient = entityClient.createSessionClient()
-    // phenylのrestAPI作成
+    /** phenylのrestAPI作成
+     * functiongroupeでAPIの挙動を指定している
+     *      ぞれぞれの挙動をfunctionとして指定して渡している？
+     * 2こめの引数はDBのclientを渡している
+     *      これでデータベース操作が可能になる
+    */
     const restApiHandler = PhenylRestApi.createFromFunctionalGroup(functionalGroup, {
         client: entityClient,
         sessionClient,
     })
 
     const app = express()
+    // CORSを全面的に許可する
     app.use(cors()) // やる
+
+    //　上のハンドラを元に実際にミドルウェアを作る
     app.use(createPhenylMiddleware({ restApiHandler })) // restApiはnonUser一個、カスタムなしで作る
 
     app.listen(port, async () => {
