474bd34ca163d0f87baf47345f6e9ee657005695
saji-ryu <rsajiki@gmail.com>
2018-06-09 18:06:33 +0900

    definitionsの部分 ・全部trueで返すUserを作成

---
+++ modules/phenyl-demo-mbaas/src/definition/UserDefinition.js
@@ -0,0 +1,19 @@
+// @flow
+import type { EntityClient } from "phenyl-interfaces";
+import { StandardUserDefinition } from "phenyl-standards";
+import type { EntityMap } from "phenyl-demo-interfaces";
+
+export default class AdminDefinition extends StandardUserDefinition<
+  EntityMap,
+  AuthSetting
+> {
+  constructor(entityClient: EntityClient<EntityMap>) {
+    super({
+      entityClient: entityClient,
+      accountPropName: "email",
+      passwordPropName: "password",
+      ttl: 1000 * 60 * 60
+    });
+  }
+  authorization = true;
+}
