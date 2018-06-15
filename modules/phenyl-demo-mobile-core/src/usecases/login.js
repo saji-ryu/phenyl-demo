9605e38e41adaeb2bc748ca506fad0cbc47da1ea
saji-ryu <rsajiki@gmail.com>
2018-06-15 12:00:33 +0900

    理解をコメントで書く

--- modules/phenyl-demo-mobile-core/src/usecases/login.js
+++ modules/phenyl-demo-mobile-core/src/usecases/login.js
@@ -19,23 +19,26 @@ export const login = ({
   /** async以降の関数をそのままreturnするという意味? */
   try {
     // phenylのエラーをリセットする
+    // action.assign をstoreに投げてる？
     dispatch(
       actions.assign([
         {
           $set: { error: null },
         },
-      ]),
+      ])
     );
+    // action.loginをstoreに投げてる
     await dispatch(
       actions.login({
         entityName: "user",
         credentials: { email, password },
-      }),
+      })
     );
-
+    //getStateでstoreのStateをとり、それをgetLoggedInUser・・・でフィルタ（今はそのまま流してる）
     const user = getLoggedInUserEntity(getState());
-    const versionId = getLoggedInUserVersionId(getState());
+    const versionId = getLoggedInUserVersionId(getState()); 
     console.log({ user, versionId });
+    //actions.followを投げる
     await dispatch(actions.follow("user", user, versionId));
   } catch (e) {
     console.error(e);
