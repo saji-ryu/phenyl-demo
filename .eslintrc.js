295ac3115fd4f10167ecf215099c09dfef1439c0
yamatatsu <the.kinnikuman@gmail.com>
2018-07-04 12:19:13 +0900

    fix: eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: [
+    "eslint:recommended",
+    "standard",
+    "plugin:react/recommended",
+    "plugin:react-native/all",
+    "plugin:import/recommended",
+    "plugin:prettier/recommended",
+  ],
+
+  plugins: ["prettier", "flowtype", "react", "react-native"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "no-console": "off", // プロダクトではやらないほうがいいよ
+    "no-multi-spaces": "off",
+    "comma-dangle": "off",
+    "react/display-name": "off",
+    "react/jsx-boolean-value": "error",
+    "react/no-redundant-should-component-update": "error",
+    "react/no-typos": "error",
+    "react/no-unused-state": "error",
+    "react/no-unused-prop-types": "error",
+    "react-native/no-color-literals": "warn",
+    "flowtype/define-flow-type": 1,
+    "flowtype/use-flow-type": 1,
+    "prefer-default-export": "off",
+    "import/default": "off",
+    "import/no-duplicates": "error",
+    "prettier/prettier": "error",
+  },
+};
